3021f75d104cd03650cdebe44264a0690a701695
bianavic <bianavic@gmail.com>
2025-02-02 18:31:44 -0300

    chore: import bianavic/utils_secret/pkg/events and test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bianavic/GOPRIVATE
 
 go 1.23.0
+
+require github.com/bianavic/utils_secret v0.0.0-20250202212629-1ff6398089fa
--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/bianavic/fullcycle_eventos/pkg/events"
+
+	"github.com/bianavic/utils_secret/pkg/events"
 )
 
-// pkg/events/event_dispatcher.go:1
 func main() {
 	eventDispatcher := events.NewEventDispatcher()
 	fmt.Println(eventDispatcher)
